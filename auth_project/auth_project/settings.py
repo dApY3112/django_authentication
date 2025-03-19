972fd5fe5b3aceea43707931777b2683fdaed300
dApY3112 <98270020+dApY3112@users.noreply.github.com>
2025-03-20 00:48:47 +0200

    Update settings.py

--- auth_project/auth_project/settings.py
+++ auth_project/auth_project/settings.py
@@ -76,16 +76,7 @@ WSGI_APPLICATION = 'auth_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_authentication',
-        'USER': 'Nhan',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
